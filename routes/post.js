affd1160d4f421f89dbcbaaeb10b98bd9fb2559d
Sy2tema <bood00142@gmail.com>
2023-09-04 22:10:40 +0900

    feat: 좋아요 추가 및 삭제 API 구현

--- routes/post.js
+++ routes/post.js
@@ -1,17 +1,37 @@
 const express = require('express');
-const { Post } = require('../models');
+const { Post, Image, User, Comment } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
-router.post('/', isLoggedIn, async (req, res, next) => {
+router.post('/', isLoggedIn, async (req, res, next) => { // POST /post
     try {
         const post = await Post.create({
             content: req.body.content,
             UserId: req.user.id,
         });
 
-        res.status(201).json(post);
+        const fullPost = await Post.findOne({
+            where: { id: post.id },
+            include: [{
+                model: Image,
+            }, {
+                model: Comment,
+                include: [{
+                    model: User, // 댓글 작성자
+                    attributes: ['id', 'nickname'],
+                }]
+            }, {
+                model: User, // 게시글 작성자
+                attributes: ['id', 'nickname'],
+            }, {
+                model: User, // 좋아요 누른 유저
+                as: 'Likers',
+                attributes: ['id'],
+            }],
+        })
+
+        res.status(201).json(fullPost);
     } catch (error) {
         console.error(error);
         next(error);
@@ -28,11 +48,51 @@ router.post('/:postId/comment', isLoggedIn, async (req, res, next) => { // POST 
 
         const comment = await Comment.create({
             content: req.body.content,
-            PostId: req.params.postId,
+            PostId: parseInt(req.params.postId, 10),
             UserId: req.user.id,
         });
 
-        res.status(201).json(comment);
+        const fullComment = await Comment.findOne({
+            where: { id: comment.id },
+            include: [{
+                model: User,
+                attributes: ['id', 'nickname'],
+            }],
+        })
+
+        res.status(201).json(fullComment);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.patch('/:postId/like', isLoggedIn, async (req, res, next) => { // PATCH /post/1/like
+    try {
+        const post = await Post.findOne({
+            where: { id: req.params.postId }
+        });
+
+        if (!post) return res.status(403).send('Post not exist');
+
+        await post.addLikers(req.user.id);
+        res.json({ PostId: post.id, UserId: req.user.id });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.delete('/:postId/like', isLoggedIn, async (req, res, next) => { // DELETE /post/1/like
+    try {
+        const post = await Post.findOne({
+            where: { id: req.params.postId }
+        });
+
+        if (!post) return res.status(403).send('Post not exist');
+
+        await post.removeLikers(req.user.id);
+        res.json({ PostId: post.id, UserId: req.user.id });
     } catch (error) {
         console.error(error);
         next(error);
