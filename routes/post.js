e45767148b8ec229cc11ad659044aa9cbe0b3659
Sy2tema <bood00142@gmail.com>
2023-10-12 01:56:51 +0900

    feat: 이미지 업로드 API 구현

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 const { Post, Image, User, Comment } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
+
+try {
+    fs.accessSync('uploads');
+} catch (error) {
+    console.log("uploads folder is not exist. create folder");
+    fs.mkdirSync('uploads');
+}
+
 
 router.post('/', isLoggedIn, async (req, res, next) => { // POST /post
     try {
@@ -36,6 +47,27 @@ router.post('/', isLoggedIn, async (req, res, next) => { // POST /post
         console.error(error);
         next(error);
     }
+});
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads');
+        },
+        filename(req, file, done) { // sample.png
+            const extend = path.extname(file.originalname); // .png
+            const basename = path.basename(file.originalname, extend); // sample
+            done(null, basename + new Date().getTime() + extend); // sample20231009.png
+        }
+    }),
+    limits: { fileSize: 20 * 1024 * 1024 }, // 20MB
+});
+
+// 이미지를 하나만 올리고 싶으면 single메서드를 사용하며 이미지를 사용하지 않는다면 None을 사용한다
+// 추가적으로 파일 input이 두 개 있을 경우에는 Fields를 쓴다
+router.post('/images', isLoggedIn, upload.array('image'), (req, res, next) => {
+    console.log(req.files);
+    res.json(req.files.map((value) => value.filename));
 });
 
 router.post('/:postId/comment', isLoggedIn, async (req, res, next) => { // POST /post/1/comments
